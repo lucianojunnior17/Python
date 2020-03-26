6579d7076dc4709ec907b0cf1b159fda325fdfee
Luciano Lima Do Nascimento Junior <54371130+lucianojunnior17@users.noreply.github.com>
2020-03-26 17:27:28 -0300

    Delete ex033.py

--- ex033.py
+++
@@ -1,9 +0,0 @@
-a = int(input('Digite o primeiro número : '))
-b = int(input('Digite o Segundo número : '))
-c = int(input("Digite o terceiro Número : "))
-menor = a
-if b<a and b<c:
-    menor = b
-if c<a and c<b:
-        menor = c
-print('O menor valor é {}'.format(menor))
