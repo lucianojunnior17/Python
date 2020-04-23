69e1f782730bbb3b08138931c4319388a432817f
Luciano Lima Do Nascimento Junior <54371130+lucianojunnior17@users.noreply.github.com>
2020-04-23 16:05:55 -0300

    Aula 63
    Aula 63 Curso de Python Gustavo Guanabara

---
+++ Curso_Guanabara/aula63.py
@@ -0,0 +1,15 @@
+####sequencia de Fibonacci #######
+print('======Sequencia de Fibonacci====== ')
+n = int(input('Quantos termos você quer mostrar ? '))
+t1 = 0
+t2 = 1
+print('-'*100)
+print('{} {} '.format(t1, t2), end='')
+cont = 3 
+while cont <= n :
+    t3 = t1+t2
+    print(' {} '.format(t3), end='')
+    t1 = t2
+    t2 = t3
+    cont +=1
+print(' FIM')
