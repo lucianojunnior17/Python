eafb27e218b790c084d730aa7a6cb427ec28f893
Luciano Lima Do Nascimento Junior <54371130+lucianojunnior17@users.noreply.github.com>
2020-05-05 16:40:27 -0300

    Aula 16 Tuplas
    Curso Python Gustavo Guanabara

---
+++ Curso_Guanabara/aula16_Tuplas_2.py
@@ -0,0 +1,14 @@
+a = (2,5,4)
+b = (5,8,1,2)
+c = a +b
+print(a)
+print(b)
+print(a+b)# vai misturar as tuplas
+print(len(c))#tamanho da tupla C
+print(c.count(5)) # Quantas vezes o Num 5 aparece
+print(c)
+print(c.index(8)) # Qual posição do número 8 
+
+pessoa = ('Luciano' , 39, 'M' ,99.88)
+#del (pessoa) Deleta a tupka
+print(pessoa)
