add26cb704210b58a559a71bf41bff06afab09e9
Luciano Lima Do Nascimento Junior <54371130+lucianojunnior17@users.noreply.github.com>
2020-07-15 22:38:24 -0300

    Update (aluguel Carro)ex015.py

--- Curso_Guanabara/(aluguel Carro)ex015.py
+++ Curso_Guanabara/(aluguel Carro)ex015.py
@@ -1,5 +1,5 @@
+import emoji
 dias = int(input('Quanos dias o carro foi  Alugados ? : '))
 km = float(input('Quantos Km Rodados ? : '))
 pago = (dias * 60) + (km * 0.15)
 print('O total  é de R$ {:.2f}'.format(pago))
-import emoji
