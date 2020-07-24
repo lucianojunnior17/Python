677ff0494b6347e853de3862168b399480ffa81d
Luciano Junior <engluciano.junior@gmail.com>
2020-07-24 15:52:18 -0300

    Aula 77

---
+++ Curso_Guanabara/aula77.py
@@ -0,0 +1,10 @@
+#########Exercício 77 Curso de Python###########
+##########CONTADOR DE VOGAIS##############################
+palavras = ('Aprender ', 'programar', 'Computacao', 'Python','curso',
+'gratis', 'engenheiro', 'programador')
+
+for p in palavras:
+    print(f'\n na palavra {p.upper()} temos', end=' ')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
