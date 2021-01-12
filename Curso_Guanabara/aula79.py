ef4dfb5d5494b0287faa7ab162675ac5450cb705
Luciano Junior <engluciano.junior@gmail.com>
2021-01-12 02:52:53 -0300

    Exercicio 79

---
+++ Curso_Guanabara/aula79.py
@@ -0,0 +1,19 @@
+###################EXERCICIO 79 ########################
+########### CURSO DE PYTOHN MUNDO 3 ####################
+########### PROF. GUANABARA ############################
+
+numero = list()
+while True:
+    n = int(input('Digite um valor : '))
+    if n not in numero:
+        numero.append(n)
+        print('Valor adicionado com sucesso !!!')
+    else:
+        print('Valor Duplicado não adicionado : ')
+    r = str(input('Quer continuar ? '))
+    if r in 'Nn':
+        break
+
+print('=='*30)
+numero.sort()
+print(f'Você digitou os valores {numero}')
