76eea1af4ee7d83f07196a3d44cfe3a94ded99d7
Luciano Junior <engluciano.junior@gmail.com>
2021-01-16 04:12:47 -0300

    aula80 update

--- Curso_Guanabara/aula80.py
+++ Curso_Guanabara/aula80.py
@@ -2,3 +2,18 @@
 ########### CURSO DE PYTOHN MUNDO 3 ####################
 ########### PROF. GUANABARA ############################
 
+lista = []
+for c in range(0,5):
+    n = int(input('Digite um valor'))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos,n)
+                break
+            pos += 1
+
+print('=='*30)
+print(f'Os valores difitedos em ordem foram {lista}')
